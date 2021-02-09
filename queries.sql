258d6cb174f8730e735faec4c8725faee834c8d8
rajandk25 <rajandk25@users.noreply.github.com>
2021-02-08 19:38:07 -0600

    completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 ## Part 1: Test it with SQL
+id: int
+employer: varchar
+name: varchar
+skills: varchar
+employer_id: int
+jobs_id: int
 
 ## Part 2: Test it with SQL
+Select name FROM employer WHERE location = "Saint Louis";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
+
 
 ## Part 4: Test it with SQL
+SELECT distinct skill.name, skill.description FROM job_skills
+INNER JOIN skill ON skill.id = job_skills.skills_id ORDER BY skill.name ASC;
